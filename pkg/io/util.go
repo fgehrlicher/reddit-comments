d50d252295b28284812e841f8060fa3eb03c6b0f
Fabian Gehrlicher <fabian.gehrlicher@outlook.de>
2021-01-11 19:44:59 +0100

    Add io package

---
+++ pkg/io/util.go
@@ -0,0 +1,22 @@
+package io
+
+import (
+	"io/ioutil"
+)
+
+func GetAllFilesInDir(dir string) ([]string, error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var result []string
+	for _, file := range files {
+		if !file.IsDir() {
+			result = append(result, file.Name())
+		}
+	}
+
+	return result, nil
+}
+
